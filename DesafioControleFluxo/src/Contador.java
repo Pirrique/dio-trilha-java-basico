467d71f938723b32f28a01e5b5552d76221f0853
pirrique <henrique_xbos@hotmail.com>
2023-10-16 21:04:55 -0300

    commit atualizacao1

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,20 @@
-public class Contador {
+import java.util.Scanner;
+public class Contador { 
     public static void main(String[] args) {
-        System.out.println("Teste de coneccao e ajustando diretorio.");
+        int num1;
+        int num2;
+        int cont = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Olá. Por favor digite o primero número.");
+        num1 = scan.nextInt();
+        System.out.println("Agora, digite o segundo número.");
+        num2 = scan.nextInt(); 
+        System.out.println("-------------------------");
+        System.out.println("Os números iterados são:");
+        for (int i = (num1 + 1); i <= num2; i++) {
+            System.out.println(i);
+            cont++;    
+        }
+        System.out.println("A quantidade de iterações é: " + cont + " números.");
     }
 }
